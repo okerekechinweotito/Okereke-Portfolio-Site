e011625a95533a601241bf37f405f6508018e2ac
okerekechinweotito <you@example.com>
2021-04-06 01:30:57 +0100

    rewind

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+/* import App from "./App"; */
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     {" "}
-    <App />
+    {/*   <App /> */}
+    <div>You have been blocked from viewing this Site</div>
   </React.StrictMode>,
   document.getElementById("root")
 );
